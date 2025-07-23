fdf2b599c4b749c707501a90645459535ac97893
Jesus Gonzalez <jrazielgsosa@gmail.com>
2025-07-22 22:40:01 -0400

    feat: notifications

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from pynput import mouse, keyboard
 from threading import Lock, Thread
+from notifypy import Notify
 
 mutex = Lock()
 mouse_controller = mouse.Controller()
@@ -17,12 +18,17 @@ seconds_delay = 0.1
 running = False
 quit_flag = False
 
+notification = Notify()
+
+
 def start_clicking():
     global running
     if running is not True:
         with mutex:
             running = True
             print("Started!")
+            notification.message = "Began clicking!"
+            _ = notification.send(block=False)
             
 
 def stop_clicking():
@@ -30,7 +36,8 @@ def stop_clicking():
     if running is True:
         with mutex:
             running = False
-            print("Stopped!")
+            notification.message = "Stopped clicking..."
+            _ = notification.send(block=False)
 
 def click():
     global running
@@ -62,6 +69,9 @@ threads = [hotkey_listener_thread, clicking_thread]
 hotkey_listener_thread.start()
 clicking_thread.start()
 
+notification.title = "Doight"
+notification.message = "Started program..."
+_ = notification.send(block=False)
 
 print("Running...")
 
@@ -69,3 +79,6 @@ for t in threads:
     t.join()
 
 print("Exiting gracefully...")
+
+notification.message = "Quitting..."
+_ = notification.send(block=False)
